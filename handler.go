64ad01fe1a7b9d126b9eafa0ec8f62ffeb9c3c4b
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-22 05:21:42 +0000

    Handler added

---
+++ handler.go
@@ -0,0 +1,10 @@
+package apex
+
+import "net/http"
+
+// Handler represents a type that can register handlers for a given HTTP verb
+// and path.
+type Handler interface {
+	http.Handler
+	Handle(method, path string, handler http.Handler)
+}
